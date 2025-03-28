d10956cb4d8084baa5011b24f4cf0fb687a8725d
Nguyễn Sỹ Nguyên <contact.nguyensynguyen@gmail.com>
2025-03-28 10:21:21 +0700

    [032825] Create SapXepMangBangComparator.java
    Code mẫu để minh họa việc sắp xếp một mảng đối tượng bằng Comparator

---
+++ SapXepMangBangComparator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // Khai bao mot mang doi tuong
+        Integer[] a = {32, 4, 53, 483, 12, 76, 223};
+
+        // Sap xep mang theo thu tu tang dan
+        Arrays.sort(a, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) { // Ham tra ve mot so am va mot so duong
+
+                // Neu return so am thi o1 dung truoc o2 va nguoc lai
+                if (o1 < o2) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+
+            }
+        });
+
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+}
